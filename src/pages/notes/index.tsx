b5aff6cf31cb7a04118f465582219060efdefde1
thgee <thgeee@naver.com>
2025-02-13 01:02:54 +0900

    feat: 노트 모아보기 초기화면 구현

---
+++ src/pages/notes/index.tsx
@@ -0,0 +1,28 @@
+import Card from "../../../views/notes/card";
+import Goal from "../../../views/notes/goal";
+import { notes } from "./dummy";
+
+export default function NotesPage() {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <div className="flex max-w-[792px] flex-col gap-4 p-4 sm:pl-[84px] md:pl-[360px]">
+        <h1 className="text-lg font-semibold text-slate-900">노트 모아보기</h1>
+        <Goal />
+
+        {/* list */}
+        <div className="flex flex-col gap-4">
+          {notes.map(({ id, title, todo }) => (
+            <Card key={id}>
+              <Card.Header />
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Divider />
+                <Card.Content todo={todo} />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
