8ff6fee4ef4179dfed02548c4b2538fcafa73f1b
thgee <thgeee@naver.com>
2025-02-12 16:50:07 +0900

    feat: dropdown 공통컴포넌트로 분리

--- src/components/atoms/dropdown/dropdown.tsx
+++ src/components/atoms/dropdown/dropdown.tsx
@@ -1,9 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import Link from "next/link";
 
 import { cn } from "@/utils/cn";
-
-const dropdownMenus = ["수정하기", "삭제하기"];
 
 const dropdownVariants = cva(
   "flex w-fit flex-col gap-3 rounded-xl text-slate-700 shadow-lg z-10 bg-white",
@@ -17,28 +14,35 @@ const dropdownVariants = cva(
   },
 );
 
-const textVariants = cva("font-normal", {
-  variants: {
-    size: {
-      md: "text-lg",
-      sm: "text-sm",
+const textVariants = cva(
+  "font-normal cursor-pointer rounded-md p-2 hover:bg-gray-100",
+  {
+    variants: {
+      size: {
+        md: "text-lg",
+        sm: "text-sm",
+      },
     },
   },
-});
+);
 
-// variants 속성들을 props로 필수로 받도록 함
 interface DropdownProps
   extends Required<VariantProps<typeof dropdownVariants>> {
   className?: string;
+  items: { label: string; onClick: () => void }[];
 }
 
-function Dropdown({ size, className }: DropdownProps) {
+function Dropdown({ size, className, items }: DropdownProps) {
   return (
     <div className={cn(dropdownVariants({ size }), className)}>
-      {dropdownMenus.map((menu, idx) => (
-        <Link href={"#"} className={cn(textVariants({ size }))} key={idx}>
-          {menu}
-        </Link>
+      {items.map((item, idx) => (
+        <div
+          key={idx}
+          className={cn(textVariants({ size }))}
+          onClick={item.onClick}
+        >
+          {item.label}
+        </div>
       ))}
     </div>
   );
