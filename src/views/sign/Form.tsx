d94b63f20764cddfd61be05a5b422e315c8ba780
Ralto13 <doorumi13@gmail.com>
2025-02-12 16:41:33 +0900

    feat: 로그인/회원가입 기능 구현

--- src/views/sign/Form.tsx
+++ src/views/sign/Form.tsx
@@ -1,18 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+import type { FieldValues } from "react-hook-form";
 import { FormProvider, useForm } from "react-hook-form";
 
-import { fieldSet } from "./fieldSet";
+import signAxios from "@/apis/sign/";
+import type { LoginRequest, UserCreateRequst } from "@/types/User";
 
-import Input from "./Input";
+import { fieldSet } from "./fieldSet";
 import type { Path } from "./index";
+import Input from "./Input";
 
 const Form = ({ path }: { path: Path }) => {
   const hookForm = useForm();
   const field = fieldSet[path];
+  const apiUrl = path === "signup" ? "/user" : "/auth/login";
+
+  const [message, setMessage] = useState<string | null>(null);
+
+  const onSubmit = async (
+    formData: FieldValues | UserCreateRequst | LoginRequest,
+  ) => {
+    try {
+      const res = await signAxios.post(apiUrl, formData);
+      if (path === "login") {
+        window.location.href = "/dashboard";
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setMessage(error.response?.data.message);
+      }
+    }
+  };
 
   return (
     <FormProvider {...hookForm}>
       <form
-        onSubmit={hookForm.handleSubmit((data) => console.log(data))}
+        onSubmit={hookForm.handleSubmit(onSubmit)}
         className="mx-4 mt-10 sm:mx-13 md:mx-160"
       >
         {field.map((item) => (
@@ -32,6 +55,7 @@ const Form = ({ path }: { path: Path }) => {
           className="mt-10 block h-12 w-full rounded-xl bg-slate-400 text-center text-base font-semibold text-white"
           value={path === "signup" ? "회원가입하기" : "로그인하기"}
         />
+        <div className="mt-5 text-center">{message}</div>
       </form>
     </FormProvider>
   );
