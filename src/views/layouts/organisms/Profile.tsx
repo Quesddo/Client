215893238c388a91f2e0378ad2aa82caec12870b
eunung <pandamon24@naver.com>
2025-02-12 15:07:44 +0900

    style: profile 이미지 fill 속성 적용

--- src/views/layouts/organisms/Profile.tsx
+++ src/views/layouts/organisms/Profile.tsx
@@ -5,14 +5,8 @@ import profile from "@public/icons/profile.png";
 export default memo(function Profile() {
   return (
     <section className="flex items-center gap-2 pt-4 pb-6 sm:gap-3">
-      <div className="h-[32px] w-[32px] sm:h-[64px] sm:w-[64px]">
-        <Image
-          src={profile}
-          alt="프로필"
-          width={64}
-          height={64}
-          className="h-full w-full"
-        />
+      <div className="relative h-[32px] w-[32px] sm:h-[64px] sm:w-[64px]">
+        <Image src={profile} alt="프로필" fill />
       </div>
       <div className="flex flex-1 items-end justify-between sm:flex-col sm:items-start sm:gap-2">
         <div className="flex flex-col">
