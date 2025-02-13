b392fd299133e8c78b1fe097bc12300713275ad0
eunung <eunung24@gmail.com>
2025-02-13 16:43:21 +0900

    feat: 회원정보 hooks 연동

--- src/views/layouts/organisms/Profile.tsx
+++ src/views/layouts/organisms/Profile.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import { memo } from "react";
+
+import { useFetchUser } from "@/hooks/user/useFetchUser";
 import profile from "@public/icons/profile.png";
 
 export default memo(function Profile() {
+  const { data } = useFetchUser();
+
   return (
     <section className="flex items-center gap-2 pt-4 pb-6 sm:gap-3">
       <div className="relative h-[32px] w-[32px] sm:h-[64px] sm:w-[64px]">
@@ -11,10 +15,10 @@ export default memo(function Profile() {
       <div className="flex flex-1 items-end justify-between sm:flex-col sm:items-start sm:gap-2">
         <div className="flex flex-col">
           <span className="text-xs font-semibold text-slate-800 sm:text-sm">
-            체다치즈
+            {data?.name || ""}
           </span>
           <span className="text-xs font-medium text-slate-600 sm:text-sm">
-            chedacheese@slid.kr
+            {data?.email || ""}
           </span>
         </div>
         <button className="text-xs font-medium text-slate-400">로그아웃</button>
