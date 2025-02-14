cf098f91ed4cb3d0d7bb883bcc2bb06fc4abf8db
Ralto13 <doorumi13@gmail.com>
2025-02-14 15:13:05 +0900

    refactor: field-set의 키값 중복 분리

--- src/views/sign/field-set.ts
+++ src/views/sign/field-set.ts
@@ -1,3 +1,18 @@
+const LOGIN = [
+  {
+    label: "이메일",
+    name: "email",
+    type: "text",
+    placeholder: "이메일을 입력해주세요",
+  },
+  {
+    label: "비밀번호",
+    name: "password",
+    type: "password",
+    placeholder: "비밀번호를 입력해주세요",
+  },
+];
+
 const FIELD_SET = {
   signup: [
     {
@@ -6,18 +21,7 @@ const FIELD_SET = {
       type: "text",
       placeholder: "이름을 입력해주세요",
     },
-    {
-      label: "이메일",
-      name: "email",
-      type: "text",
-      placeholder: "이메일을 입력해주세요",
-    },
-    {
-      label: "비밀번호",
-      name: "password",
-      type: "password",
-      placeholder: "비밀번호를 입력해주세요",
-    },
+    ...LOGIN,
     {
       label: "비밀번호 확인",
       name: "confirmPassword",
@@ -25,19 +29,6 @@ const FIELD_SET = {
       placeholder: "비밀번호 확인을 입력해주세요",
     },
   ],
-  login: [
-    {
-      label: "이메일",
-      name: "email",
-      type: "text",
-      placeholder: "이메일을 입력해주세요",
-    },
-    {
-      label: "비밀번호",
-      name: "password",
-      type: "password",
-      placeholder: "비밀번호를 입력해주세요",
-    },
-  ],
+  login: LOGIN,
 };
 export default FIELD_SET;
