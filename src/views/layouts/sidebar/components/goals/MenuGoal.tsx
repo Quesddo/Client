9954bef1880b95daecde4cb870aed621925373d1
euNung24 <eunung24@gmail.com>
2025-02-17 14:27:27 +0900

    feat(TODO-82): 사이드 바 목표 api 연동 (#63)
    * feat: 목표 hooks 연동
    
    * feat: menuitem 컴포넌트 memo 적용
    
    - 목표 데이터에 따른 리렌터링 방지
    
    * feat: 목표 에러 시 처리

--- src/views/layouts/sidebar/components/goals/MenuGoal.tsx
+++ src/views/layouts/sidebar/components/goals/MenuGoal.tsx
@@ -1,12 +1,26 @@
-import { memo } from "react";
+import { FormEvent, memo, useState } from "react";
+
+import { useCreateGoal } from "@/hooks/goal/useCreateGoal";
+import { useFetchGoals } from "@/hooks/goal/useFetchGoals";
 import flag from "@public/icons/flag.png";
 
 import MenuItem from "../MenuItem";
 import TabSideMenuList from "./TabSideMenuList";
 
-const items = ["hihihihi"];
-
 export default memo(function MenuGoal() {
+  const { data, isError, error } = useFetchGoals();
+  const mutation = useCreateGoal();
+  const [goalInput, setGoalInput] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGoalInput(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    mutation.mutate(goalInput);
+  };
+
   return (
     <section className="flex min-h-0 flex-1 flex-col gap-3 pt-3">
       <div className="flex justify-between">
@@ -14,8 +28,25 @@ export default memo(function MenuGoal() {
         <button className="sm:hidden">새 목표</button>
       </div>
       <div className="flex min-h-0 flex-1 flex-col gap-6">
-        <TabSideMenuList items={items} />
-        <button className="h-[44px]">새 목표</button>
+        {!isError ? (
+          <>
+            <TabSideMenuList items={data?.goals || []} />
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={goalInput}
+                onChange={handleInput}
+                className="border border-slate-300"
+              />
+              <button>제출</button>
+            </form>
+            <button type="button" className="h-[44px]">
+              새 목표
+            </button>
+          </>
+        ) : (
+          <p>에러 발생: {(error as Error).message}</p>
+        )}
       </div>
     </section>
   );
--- src/views/layouts/sidebar/components/goals/TabSideMenuList.tsx
+++ src/views/layouts/sidebar/components/goals/TabSideMenuList.tsx
@@ -1,14 +1,15 @@
 import TabSideMenuItem from "./TabSideMenuItem";
+import { TeamIdGoalsGet200Response } from "@/types/types";
 
 interface TabSideMenuItemProps {
-  items: string[];
+  items: TeamIdGoalsGet200Response["goals"];
 }
 
 export default function TabSideMenuList({ items }: TabSideMenuItemProps) {
   return (
     <ul className="flex-1 overflow-auto">
-      {items.map((item, i) => (
-        <TabSideMenuItem key={i} content={item} />
+      {items.map((item) => (
+        <TabSideMenuItem key={item.id} content={item.title} />
       ))}
     </ul>
   );
