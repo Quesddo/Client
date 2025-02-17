ef2f5b05697365548b2f5d4e2bd0135200d030ad
eunung <eunung24@gmail.com>
2025-02-17 13:20:16 +0900

    feat: menuGoal 하위 컴포넌트 분리

--- src/views/layouts/molecules/GoalCreationForm.tsx
+++ src/views/layouts/molecules/GoalCreationForm.tsx
@@ -1,15 +1,22 @@
 import { memo } from "react";
 
+import { cn } from "@/utils/cn";
+
 type GoalCreationFormProps = {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  isVisible: boolean;
 };
 
 export default memo(function GoalCreationForm({
   onSubmit,
+  isVisible,
 }: GoalCreationFormProps) {
   return (
     <form
-      className="box-border flex w-full flex-col gap-2 rounded-lg bg-white px-2 text-sm font-medium"
+      className={cn(
+        "box-border flex w-full flex-col gap-2 rounded-lg bg-white px-2 text-sm font-medium",
+        { hidden: !isVisible },
+      )}
       onSubmit={onSubmit}
     >
       <div className="flex w-full items-center gap-1">
--- src/views/layouts/organisms/MenuGoal.tsx
+++ src/views/layouts/organisms/MenuGoal.tsx
@@ -1,53 +1,72 @@
-import { FormEvent, memo, useState } from "react";
+import flag from "@public/icons/flag.png";
+import { FormEventHandler, memo, useRef, useState } from "react";
 
 import { useCreateGoal } from "@/hooks/goal/useCreateGoal";
 import { useFetchGoals } from "@/hooks/goal/useFetchGoals";
-import flag from "@public/icons/flag.png";
+import { cn } from "@/utils/cn";
 
+import AddButton from "../atoms/AddButton";
 import MenuItem from "../atoms/MenuItem";
+import GoalCreationForm from "../molecules/GoalCreationForm";
 import TabSideMenuList from "../molecules/TabSideMenuList";
 
 export default memo(function MenuGoal() {
   const { data, isError, error } = useFetchGoals();
   const mutation = useCreateGoal();
-  const [goalInput, setGoalInput] = useState("");
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setGoalInput(e.currentTarget.value);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [showForm, setShowForm] = useState(false);
+
+  const handleShowForm = () => {
+    setShowForm(true);
+    ref.current?.scrollTo(0, ref.current?.scrollHeight);
   };
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit: FormEventHandler = (e) => {
     e.preventDefault();
-    mutation.mutate(goalInput);
+    const formData = new FormData(e.target as HTMLFormElement);
+    const title = formData.get("title") as string;
+
+    if (title) {
+      mutation.mutate(title);
+    }
+
+    setShowForm(false);
   };
 
   return (
-    <section className="flex min-h-0 flex-1 flex-col gap-3 pt-3">
-      <div className="flex justify-between">
-        <MenuItem title="목표" icon={flag} />
-        <button className="sm:hidden">새 목표</button>
-      </div>
-      <div className="flex min-h-0 flex-1 flex-col gap-6">
-        {!isError ? (
-          <>
-            <TabSideMenuList items={data?.goals || []} />
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={goalInput}
-                onChange={handleInput}
-                className="border border-slate-300"
-              />
-              <button>제출</button>
-            </form>
-            <button type="button" className="h-[44px]">
-              새 목표
-            </button>
-          </>
-        ) : (
-          <p>에러 발생: {(error as Error).message}</p>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="flex min-h-0 flex-1 flex-col gap-3 pt-3">
+        <div className="flex justify-between">
+          <MenuItem title="목표" icon={flag} />
+          <AddButton
+            size="xs"
+            outline
+            onClick={handleShowForm}
+            className={cn(showForm && "hidden")}
+          >
+            새 목표
+          </AddButton>
+        </div>
+        <div className="flex min-h-0 flex-1 flex-col gap-6">
+          {!isError ? (
+            <div className="flex-1 overflow-auto" ref={ref}>
+              <TabSideMenuList items={data?.goals || []} />
+              <GoalCreationForm onSubmit={handleSubmit} isVisible={showForm} />
+            </div>
+          ) : (
+            <p>에러 발생: {(error as Error).message}</p>
+          )}
+          <AddButton
+            outline
+            onClick={handleShowForm}
+            className={cn(showForm && "hidden sm:hidden")}
+          >
+            새 목표
+          </AddButton>
+        </div>
+      </section>
+    </>
   );
 });
