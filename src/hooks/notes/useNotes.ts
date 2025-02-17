241b5b08c2b0e876572a7d82d1e75b5872237f32
thgee <thgeee@naver.com>
2025-02-17 17:17:33 +0900

    feat: useNotes를 useInfiniteQuery 사용하여 구현

--- src/hooks/notes/useNotes.ts
+++ src/hooks/notes/useNotes.ts
@@ -1,14 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import instance from "@/apis/apiClient";
 import { TeamIdNotesGet200Response, teamIdNotesGetParams } from "@/types/types";
 
-export const useNotes = (params?: teamIdNotesGetParams) => {
-  return useQuery<TeamIdNotesGet200Response>({
-    queryKey: ["notes"],
-    queryFn: async () => {
-      const { data } = await instance.get("/notes", { params });
-      return data;
+export const useNotes = (goalId: number) => {
+  return useInfiniteQuery<TeamIdNotesGet200Response>({
+    queryKey: ["notes", goalId],
+    queryFn: async ({ pageParam }) => {
+      const params: teamIdNotesGetParams = {
+        goalId,
+        cursor: pageParam as number,
+        size: 10,
+      };
+      return (
+        await instance.get("notes", {
+          params,
+        })
+      ).data;
     },
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    initialPageParam: null,
+    enabled: !!goalId,
   });
 };
