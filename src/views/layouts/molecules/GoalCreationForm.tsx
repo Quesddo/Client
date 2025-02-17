069b63b930c62245cb54e499701d0340ee30a24e
eunung <eunung24@gmail.com>
2025-02-17 11:44:34 +0900

    feat: goalCreationForm 컴포넌트 구현

---
+++ src/views/layouts/molecules/GoalCreationForm.tsx
@@ -0,0 +1,22 @@
+type GoalCreationFormProps = {
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export default function GoalCreationForm({ onSubmit }: GoalCreationFormProps) {
+  return (
+    <form
+      className="box-border flex w-full flex-col gap-2 rounded-lg bg-white px-2 text-sm font-medium"
+      onSubmit={onSubmit}
+    >
+      <div className="flex w-full items-center gap-1">
+        <span>•</span>
+        <input
+          placeholder="목표를 입력하세요"
+          autoFocus
+          name="title"
+          className="w-full rounded bg-slate-100 p-2 text-base outline-none sm:text-sm"
+        />
+      </div>
+    </form>
+  );
+}
