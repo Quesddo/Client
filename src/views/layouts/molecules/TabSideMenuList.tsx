8ab1e6708543e52c7d3e60936f6609385ed3d553
eunung <eunung24@gmail.com>
2025-02-18 16:22:18 +0900

    feat: tabSideMenuList 무한 스크롤 기능 분리

--- src/views/layouts/molecules/TabSideMenuList.tsx
+++ src/views/layouts/molecules/TabSideMenuList.tsx
@@ -1,19 +1,56 @@
-import { memo } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { memo, useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
-import { TeamIdGoalsGet200Response } from "@/types/types";
+import instance from "@/apis/apiClient";
+import { TeamIdGoalsGet200Response, teamIdGoalsGetParams } from "@/types/types";
 
 import TabSideMenuItem from "../atoms/TabSideMenuItem";
 
-interface TabSideMenuItemProps {
-  items: TeamIdGoalsGet200Response["goals"];
-}
+export default memo(function TabSideMenuList() {
+  const { data, fetchNextPage, hasNextPage, isError, error } =
+    useInfiniteQuery<TeamIdGoalsGet200Response>({
+      queryKey: ["goals"],
+      queryFn: async ({ pageParam }) => {
+        const params: teamIdGoalsGetParams = {
+          sortOrder: "newest",
+          size: 20,
+          cursor: pageParam as number,
+        };
 
-export default memo(function TabSideMenuList({ items }: TabSideMenuItemProps) {
+        const { data } = await instance.get("/goals", { params });
+
+        return data;
+      },
+      initialPageParam: null,
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    });
+
+  const goals = data?.pages.map((page) => [...page.goals]).flat();
+
+  const ref = useRef<HTMLUListElement>(null);
+
+  const { ref: inViewRef, inView } = useInView({
+    root: ref.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
+
+  if (isError) {
+    <p>에러 발생: {(error as Error).message}</p>;
+  }
+
   return (
-    <ul className="flex-1 overflow-auto">
-      {items.map((item) => (
+    <ul className="flex min-h-0 flex-1 flex-col overflow-auto" ref={ref}>
+      {goals?.map((item) => (
         <TabSideMenuItem key={item.id} content={item.title} />
       ))}
+      {goals?.length && <li ref={inViewRef}>hi</li>}
     </ul>
   );
 });
