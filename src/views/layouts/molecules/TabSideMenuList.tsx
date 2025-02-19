eee5018d5efaef6a8a8a023bf8b0b8730ec66425
eunung <eunung24@gmail.com>
2025-02-19 09:17:26 +0900

    feat: 최초 마운트 시 inView가 true인 경우 스크롤 되지 않는 문제 처리

--- src/views/layouts/molecules/TabSideMenuList.tsx
+++ src/views/layouts/molecules/TabSideMenuList.tsx
@@ -1,20 +1,32 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { memo, useEffect, useRef } from "react";
+import { forwardRef, memo, Ref, useEffect, useRef } from "react";
 import { useInView } from "react-intersection-observer";
 
 import instance from "@/apis/apiClient";
 import { TeamIdGoalsGet200Response, teamIdGoalsGetParams } from "@/types/types";
+import { cn } from "@/utils/cn";
 
 import TabSideMenuItem from "../atoms/TabSideMenuItem";
 
-export default memo(function TabSideMenuList() {
-  const { data, fetchNextPage, hasNextPage, isError, error } =
-    useInfiniteQuery<TeamIdGoalsGet200Response>({
+export default memo(
+  forwardRef(function TabSideMenuList(_, ref: Ref<HTMLDivElement | null>) {
+    const {
+      data,
+      fetchNextPage,
+      hasNextPage,
+      isFetchingNextPage,
+      isError,
+      error,
+    } = useInfiniteQuery<
+      TeamIdGoalsGet200Response,
+      Error,
+      TeamIdGoalsGet200Response["goals"]
+    >({
       queryKey: ["goals"],
       queryFn: async ({ pageParam }) => {
         const params: teamIdGoalsGetParams = {
           sortOrder: "newest",
-          size: 20,
+          size: 10,
           cursor: pageParam as number,
         };
 
@@ -24,33 +36,67 @@ export default memo(function TabSideMenuList() {
       },
       initialPageParam: null,
       getNextPageParam: (lastPage) => lastPage.nextCursor,
+      select: (data) => data.pages.map((page) => [...page.goals]).flat(),
     });
 
-  const goals = data?.pages.map((page) => [...page.goals]).flat();
+    const ulRef = useRef<HTMLUListElement>(null);
 
-  const ref = useRef<HTMLUListElement>(null);
+    const { ref: inViewRef, inView } = useInView({
+      root: (ref as React.RefObject<HTMLDivElement>)?.current,
+      threshold: 1,
+    });
 
-  const { ref: inViewRef, inView } = useInView({
-    root: ref.current,
-    threshold: 1,
-  });
+    useEffect(() => {
+      const divEl = (ref as React.RefObject<HTMLDivElement>)?.current;
+      const ulEl = ulRef?.current;
 
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
+      if (!divEl || !ulEl) {
+        return;
+      }
+
+      if (
+        inView &&
+        hasNextPage &&
+        !isFetchingNextPage &&
+        ulEl.scrollHeight <= divEl.clientHeight
+      ) {
+        fetchNextPage();
+      }
+    }, [inView, hasNextPage, isFetchingNextPage]);
+
+    useEffect(() => {
+      const divEl = (ref as React.RefObject<HTMLDivElement>)?.current;
+      const ulEl = ulRef?.current;
+
+      if (!divEl || !ulEl) {
+        return;
+      }
+
+      if (inView && hasNextPage && ulEl.scrollHeight > divEl.clientHeight) {
+        fetchNextPage();
+      }
+    }, [inView, hasNextPage]);
+
+    if (isError) {
+      return <p>에러 발생: {(error as Error).message}</p>;
     }
-  }, [inView, hasNextPage]);
 
-  if (isError) {
-    <p>에러 발생: {(error as Error).message}</p>;
-  }
-
-  return (
-    <ul className="flex min-h-0 flex-1 flex-col overflow-auto" ref={ref}>
-      {goals?.map((item) => (
-        <TabSideMenuItem key={item.id} content={item.title} />
-      ))}
-      {goals?.length && <li ref={inViewRef}>hi</li>}
-    </ul>
-  );
-});
+    return (
+      <div
+        className={cn(
+          "relative flex min-h-0 flex-1 flex-col overflow-hidden",
+          hasNextPage &&
+            "after:absolute after:inset-x-0 after:bottom-0 after:h-1/3 after:max-h-[80px] after:bg-[linear-gradient(180deg,rgba(255,255,255,0)_0%,rgba(255,255,255,0.6)_57%,rgba(255,255,255,0.8)_100%)]",
+        )}
+        ref={ref}
+      >
+        <ul className="min-h-0 flex-1 overflow-auto" ref={ulRef}>
+          {data?.map((goal) => (
+            <TabSideMenuItem key={goal.id} content={goal.title} />
+          ))}
+          {data?.length && <li ref={inViewRef}></li>}
+        </ul>
+      </div>
+    );
+  }),
+);
