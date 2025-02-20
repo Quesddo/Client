88f2172fe3ed9c07ddc35e5fb2e9ea1a079a7db7
euNung24 <eunung24@gmail.com>
2025-02-20 13:15:23 +0900

    feat(TODO-114): [사이드바] 목표 무한 스크롤 구현 (#80)
    * chore: react-intersection-observer 라이브러리 추가
    
    * feat: tabSideMenuList 무한 스크롤 기능 분리
    
    * style: goalCreationForm 스타일 수정
    
    * feat: 목표 생성 hooks 무한 스크롤에 맞춰 쿼리 수정
    
    * feat: 최초 마운트 시 inView가 true인 경우 스크롤 되지 않는 문제 처리
    
    * feat: 하위 컴포넌트에 맞춰 MenuGoal 컴포넌트 수정
    
    * refactor: 목표 무한 스크롤 로직 hooks로 분리
    
    * fix: ref 참조 수정
    
    * fix: childRef 제네릭 타입 수정

--- src/views/layouts/molecules/TabSideMenuList.tsx
+++ src/views/layouts/molecules/TabSideMenuList.tsx
@@ -1,19 +1,38 @@
-import { memo } from "react";
+import { ForwardedRef, forwardRef, memo, useRef } from "react";
 
-import { TeamIdGoalsGet200Response } from "@/types/types";
+import useInfiniteGoals from "@/hooks/goal/useIniniteGoals";
+import { cn } from "@/utils/cn";
 
 import TabSideMenuItem from "../atoms/TabSideMenuItem";
 
-interface TabSideMenuItemProps {
-  items: TeamIdGoalsGet200Response["goals"];
-}
+export default memo(
+  forwardRef(function TabSideMenuList(_, ref: ForwardedRef<HTMLUListElement>) {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const {
+      query: { data, isError, error, hasNextPage },
+      inViewRef,
+    } = useInfiniteGoals(containerRef, ref);
 
-export default memo(function TabSideMenuList({ items }: TabSideMenuItemProps) {
-  return (
-    <ul className="flex-1 overflow-auto">
-      {items.map((item) => (
-        <TabSideMenuItem key={item.id} content={item.title} />
-      ))}
-    </ul>
-  );
-});
+    if (isError) {
+      return <p>에러 발생: {(error as Error).message}</p>;
+    }
+
+    return (
+      <div
+        className={cn(
+          "relative flex min-h-0 flex-1 flex-col overflow-hidden",
+          hasNextPage &&
+            "after:absolute after:inset-x-0 after:bottom-0 after:h-1/3 after:max-h-[80px] after:bg-[linear-gradient(180deg,rgba(255,255,255,0)_0%,rgba(255,255,255,0.6)_57%,rgba(255,255,255,0.8)_100%)]",
+        )}
+        ref={containerRef}
+      >
+        <ul className="min-h-0 flex-1 overflow-auto" ref={ref}>
+          {data?.map((goal) => (
+            <TabSideMenuItem key={goal.id} content={goal.title} />
+          ))}
+          {data?.length && <li ref={inViewRef}></li>}
+        </ul>
+      </div>
+    );
+  }),
+);
