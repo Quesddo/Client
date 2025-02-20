471f7c5d4527ec5af53810cf4b2896ff1bda98cb
euNung24 <eunung24@gmail.com>
2025-02-20 13:15:23 +0900

    feat(TODO-114): [사이드바] 목표 무한 스크롤 구현 (#80)
    * chore: react-intersection-observer 라이브러리 추가
    
    * feat: tabSideMenuList 무한 스크롤 기능 분리
    
    * style: goalCreationForm 스타일 수정
    
    * feat: 목표 생성 hooks 무한 스크롤에 맞춰 쿼리 수정
    
    * feat: 최초 마운트 시 inView가 true인 경우 스크롤 되지 않는 문제 처리
    
    * feat: 하위 컴포넌트에 맞춰 MenuGoal 컴포넌트 수정
    
    * refactor: 목표 무한 스크롤 로직 hooks로 분리
    
    * fix: ref 참조 수정
    
    * fix: childRef 제네릭 타입 수정

--- src/views/layouts/molecules/GoalCreationForm.tsx
+++ src/views/layouts/molecules/GoalCreationForm.tsx
@@ -1,33 +1,21 @@
-import { memo } from "react";
-
-import { cn } from "@/utils/cn";
+import Input from "@/components/atoms/input/Input";
 
 type GoalCreationFormProps = {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-  isVisible: boolean;
 };
 
-export default memo(function GoalCreationForm({
-  onSubmit,
-  isVisible,
-}: GoalCreationFormProps) {
+export default (function GoalCreationForm({ onSubmit }: GoalCreationFormProps) {
   return (
     <form
-      className={cn(
-        "box-border flex w-full flex-col gap-2 rounded-lg bg-white px-2 text-sm font-medium",
-        { hidden: !isVisible },
-      )}
+      className="box-border flex w-full flex-col gap-2 rounded-lg bg-white text-sm font-medium"
       onSubmit={onSubmit}
     >
-      <div className="flex w-full items-center gap-1">
-        <span>•</span>
-        <input
-          placeholder="목표를 입력하세요"
-          autoFocus
-          name="title"
-          className="w-full rounded bg-slate-100 p-2 text-base outline-none sm:text-sm"
-        />
-      </div>
+      <Input
+        placeholder="목표를 입력하세요"
+        autoFocus
+        name="title"
+        className="rounded-[12px]"
+      />
     </form>
   );
 });
