346e919bec340b701b3752fc76d6eadb5bb9e364
thgee <thgeee@naver.com>
2025-02-21 13:28:53 +0900

    feat: 드롭다운 애니메이션 추가 및 외부클릭 로직 변경

--- src/components/atoms/dropdown/dropdown.tsx
+++ src/components/atoms/dropdown/dropdown.tsx
@@ -1,13 +1,20 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import type { Variants } from "framer-motion";
-import { motion } from "framer-motion";
-import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+
+import { animate, AnimatePresence, motion } from "framer-motion";
+import { Dispatch, SetStateAction } from "react";
 
 import { cn } from "@/utils/cn";
+import type { Variants } from "framer-motion";
 
 const dropdownMotionVariants: Variants = {
-  hidden: { opacity: 0, y: -10 },
-  visible: { opacity: 1, y: 0 },
+  hidden: {
+    opacity: 0,
+    y: -15,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 const dropdownStyleVariants = cva(
@@ -28,30 +35,54 @@ const dropdownStyleVariants = cva(
 interface DropdownProps extends VariantProps<typeof dropdownStyleVariants> {
   items: { label: string; onClick: () => void }[];
   className?: string;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function Dropdown({ size, items, className }: DropdownProps) {
+export default function Dropdown({
+  size,
+  items,
+  className,
+  isOpen,
+  setIsOpen,
+}: DropdownProps) {
   return (
-    <motion.ul
-      ref={dropdownRef}
-      variants={dropdownMotionVariants}
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      className={cn(
-        "z-10 flex w-fit flex-col rounded-xl bg-white text-slate-700 shadow-lg",
-        className,
+    <AnimatePresence>
+      {isOpen && (
+        <>
+          <motion.ul
+            variants={dropdownMotionVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            className={cn(
+              "z-2 flex w-fit flex-col rounded-xl bg-white text-slate-700 shadow-lg",
+              className,
+            )}
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            {items.map((item, idx) => (
+              <li
+                key={idx}
+                className={cn(dropdownStyleVariants({ size }))}
+                onClick={item.onClick}
+              >
+                {item.label}
+              </li>
+            ))}
+          </motion.ul>
+
+          <div
+            className="fixed inset-0 z-1 bg-transparent"
+            onClick={() => {
+              // 배경 클릭 시 닫힘
+              setIsOpen(false);
+            }}
+          />
+        </>
       )}
-    >
-      {items.map((item, idx) => (
-        <li
-          key={idx}
-          className={cn(dropdownStyleVariants({ size }))}
-          onClick={item.onClick}
-        >
-          {item.label}
-        </li>
-      ))}
-    </motion.ul>
+    </AnimatePresence>
   );
 }
