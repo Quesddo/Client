650d3032a81d3ff10ea11fa5c8de115615b3e4d0
다염 <147478174+hdayeon@users.noreply.github.com>
2025-02-21 14:08:12 +0900

    feat(TODO-137): 생성, 수정 폼 InputModal 컴파운드 구성 (#85)
    * chore: 할 일 목록에서 모달 제거 - 외부에서 자율적으로 핸들링하도록 수정
    
    * feat: 생성 수정 input모달 초안
    
    * refactor: 드래그앤 드롭 hook으로 로직 분리
    
    * refactor: 미사용 애니메이션 정리 및 모달 닫은 후 입력 값 reset 추가
    
    * fix: 공통 컴포넌트 수정으로 사용법 변경되어 불가피하게 최소한의 수정본 업데이트
    
    * fix: 모달 렌더링 위치 변경(특정 DOM 요소->body 태그)
    
    * fix: forwardRef 사용에 따른 displayName 명시 추가
    
    * style: dropdownInput 제외한 기본 스타일
    
    * fix: 새로 만든 RefInput 파일 제거, 기존 Input 수정 후 적용
    
    * feat: 파일 업로드 미리보기(이미지 - 프리뷰 / 그 외 파일 - 이름)
    
    * refactor: 닫기 버튼  SVG 컴포넌트 성능 최적화
    
    * refactor: modalContext 로직 분리

---
+++ src/components/organisms/modal/InputModal.tsx
@@ -0,0 +1,293 @@
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import {
+  ButtonHTMLAttributes,
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+  ReactNode,
+  useRef,
+} from "react";
+import ReactDOM from "react-dom";
+import { useFormContext } from "react-hook-form";
+
+import Button from "@/components/atoms/button/Button";
+import Input from "@/components/atoms/input/Input";
+import PlusIcon from "@/components/atoms/plus-icon/PlusIcon";
+import { useModalContext } from "@/contexts/InputModalContext";
+import useDragAndDrop from "@/hooks/useDragAndDrop";
+import useFilePreview from "@/hooks/useFilePreview";
+import { cn } from "@/utils/cn";
+
+export default function InputModal({ children }: { children: ReactNode }) {
+  const { isOpen } = useModalContext();
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(<div>{children}</div>, document.body);
+}
+
+const MODAL_ANIMATION = {
+  fadeIn: "animate-fadeIn",
+};
+
+function Overlay({ className }: { className?: string }) {
+  const { isOpen, closeModal } = useModalContext();
+  const { watch, reset } = useFormContext();
+  const [title, linkUrl, fileUrl] = watch(["title", "linkUrl", "fileUrl"]);
+
+  return (
+    <div
+      className={cn(
+        "fixed inset-0 z-20 flex items-center justify-center sm:bg-black/50",
+        isOpen && MODAL_ANIMATION.fadeIn,
+        className,
+      )}
+      onClick={() => {
+        if (title || linkUrl || fileUrl) {
+          if (!confirm("(팝업창)모달 닫기 확인")) return;
+        }
+        closeModal();
+        reset();
+      }}
+    />
+  );
+}
+
+function Content({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  const { isOpen } = useModalContext();
+
+  return (
+    <div
+      className={cn(
+        "fixed top-[50%] left-[50%] z-30 h-[calc(100vh-48px)] w-[calc(100vw-48px)] -translate-x-1/2 -translate-y-1/2 transform bg-white p-6 font-semibold text-slate-800",
+        isOpen && MODAL_ANIMATION.fadeIn,
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Title({ children }: { children: string }) {
+  return <h1 className="z-30 text-lg font-bold">{children}</h1>;
+}
+
+const CloseIcon = dynamic(
+  () => import("@/components/atoms/close-icon/CloseIcon"),
+  { ssr: false },
+);
+function CloseButton() {
+  const { closeModal } = useModalContext();
+  const { watch, reset } = useFormContext();
+  const [title, linkUrl, fileUrl] = watch(["title", "linkUrl", "fileUrl"]);
+  const closeConfirm = () => {
+    if (title || linkUrl || fileUrl) {
+      if (!confirm("(팝업창)모달 닫기 확인")) return;
+    }
+    closeModal();
+    reset();
+  };
+
+  return (
+    <button onClick={closeConfirm}>
+      <CloseIcon />
+    </button>
+  );
+}
+
+function Label({ children, ...props }: LabelHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <label className="mb-3 block" {...props}>
+      {children}
+    </label>
+  );
+}
+
+// ------- Form Input 컴포넌트 -------
+const ALLOWED_SIZES = ["default", "lg", "sm", null] as const;
+
+const TextInput = ({
+  size,
+  ...props
+}: Omit<InputHTMLAttributes<HTMLInputElement>, "size"> & {
+  size?: (typeof ALLOWED_SIZES)[number];
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return <Input size={size} ref={inputRef} {...props} />;
+};
+
+function FileInput({
+  onFileChange,
+}: {
+  onFileChange: (files: FileList) => void;
+}) {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const { previewFile, updateFilePreview } = useFilePreview();
+
+  const { isDragging, handleDragOver, handleDragLeave, handleDrop } =
+    useDragAndDrop({
+      onDrop: (files) => {
+        onFileChange(files);
+        updateFilePreview(files);
+
+        // input 값도 업데이트
+        if (fileInputRef.current) {
+          const dataTransfer = new DataTransfer();
+          Array.from(files).forEach((file) => dataTransfer.items.add(file));
+          fileInputRef.current.files = dataTransfer.files;
+        }
+      },
+    });
+
+  return (
+    <div
+      className="h-[184px] transition-all"
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      <label
+        htmlFor="file-upload"
+        className={cn(
+          "relative flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-xl bg-slate-50",
+          isDragging && "bg-blue-100",
+        )}
+      >
+        {previewFile ? (
+          previewFile.url ? (
+            <Image
+              fill
+              src={previewFile.url}
+              alt={previewFile.name}
+              className="h-16 w-16 rounded-md object-contain"
+            />
+          ) : (
+            <>
+              <img
+                src="/icons/uploaded.png"
+                alt="업로드파일"
+                width={24}
+                height={24}
+              />
+              <span
+                className="max-w-[90%] text-center text-sm font-normal break-words text-gray-400"
+                title={previewFile.name}
+              >
+                {previewFile.name}
+              </span>
+            </>
+          )
+        ) : (
+          <>
+            <PlusIcon color="gray" />
+            <p className="font-normal text-slate-400">파일을 업로드해주세요</p>
+          </>
+        )}
+        <input
+          ref={fileInputRef}
+          id="file-upload"
+          type="file"
+          className="hidden"
+          onChange={(e) => {
+            if (e.target.files) {
+              onFileChange(e.target.files);
+              updateFilePreview(e.target.files);
+            }
+          }}
+        />
+      </label>
+    </div>
+  );
+}
+
+function DropdownInput<T extends string | number>({
+  options,
+  ...props
+}: {
+  options: T[];
+}) {
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  return (
+    <select
+      ref={selectRef}
+      className="w-full rounded-xl bg-slate-200 p-2"
+      {...props}
+    >
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
+// -----------------------
+
+function SubmitButton({
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <Button type="submit" {...props}>
+      {children}
+    </Button>
+  );
+}
+
+function CheckButton({
+  checked,
+  onToggle,
+  children,
+}: {
+  checked: boolean;
+  onToggle: () => void;
+  children: string;
+}) {
+  return (
+    <button
+      type="button"
+      onClick={(event) => {
+        event.preventDefault();
+        onToggle();
+      }}
+      className={cn(
+        "group flex shrink-0 cursor-pointer items-center gap-[10px] rounded-lg bg-slate-100 p-2 pr-3 pl-3 text-base font-medium text-slate-800 transition duration-200",
+        checked && "bg-slate-800 text-white",
+      )}
+    >
+      <img
+        src={
+          checked
+            ? "/icons/active-check-white.png"
+            : "/icons/inactive-check.png"
+        }
+        alt={checked ? "체크됨" : "미체크"}
+        width={18}
+        height={18}
+        className="rounded-md group-hover:shadow-sm"
+      />
+      <p>{children}</p>
+    </button>
+  );
+}
+
+InputModal.Overlay = Overlay;
+InputModal.Title = Title;
+InputModal.Content = Content;
+InputModal.CloseButton = CloseButton;
+
+InputModal.Label = Label;
+InputModal.TextInput = TextInput;
+InputModal.FileInput = FileInput;
+InputModal.DropdownInput = DropdownInput;
+
+InputModal.SubmitButton = SubmitButton;
+InputModal.CheckButton = CheckButton;
