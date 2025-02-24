2ebe1ef328c0a0c9e595156d3cc626908a33fe8f
thgee <thgeee@naver.com>
2025-02-24 17:48:18 +0900

    feat: [노트 상세보기] 공통컴포넌트 분리

---
+++ src/components/atoms/goal-item/GoalItem.stories.tsx
@@ -0,0 +1,36 @@
+import GoalItem from "./GoalItem";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof GoalItem> = {
+  title: "Common/Atoms/GoalItem",
+  component: GoalItem,
+  argTypes: {
+    goal: { control: "text" },
+    iconSize: {
+      control: "radio",
+      options: ["sm", "lg"],
+    },
+    fontSize: {
+      control: "radio",
+      options: ["sm", "md", "lg"],
+    },
+    fontWeight: {
+      control: "radio",
+      options: ["medium", "semibold"],
+    },
+    gap: { control: { type: "number", min: 0, max: 20 } },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GoalItem>;
+
+export const Default: Story = {
+  args: {
+    goal: "자바스크립트로 웹 서비스 만들기",
+    iconSize: "sm",
+    fontSize: "sm",
+    fontWeight: "medium",
+  },
+};
---
+++ src/components/atoms/goal-item/GoalItem.tsx
@@ -0,0 +1,52 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import Image from "next/image";
+
+const textVariants = cva("text-base font-medium text-slate-800", {
+  variants: {
+    fontSize: {
+      sm: "text-sm",
+      md: "text-base",
+      lg: "text-lg",
+    },
+    fontWeight: {
+      medium: "font-medium",
+      semibold: "font-semibold",
+    },
+  },
+  defaultVariants: {
+    fontSize: "sm",
+    fontWeight: "medium",
+  },
+});
+
+interface GoalItemProps extends VariantProps<typeof textVariants> {
+  iconSize: "sm" | "lg";
+  gap: number;
+  goal?: string;
+}
+
+export default function GoalItem({
+  goal,
+  iconSize,
+  fontSize,
+  fontWeight,
+  gap = 6,
+}: GoalItemProps) {
+  const iconProps = {
+    src: `/icons/flag-goal-${iconSize}.png`,
+    width: iconSize === "lg" ? 40 : 24,
+    height: iconSize === "lg" ? 40 : 24,
+    alt: "flag-goal",
+    layout: "fixed",
+  };
+
+  return (
+    <div
+      className={`flex w-fit items-center justify-center`}
+      style={{ gap: `${gap}px` }}
+    >
+      <Image {...iconProps} />
+      <span className={textVariants({ fontWeight, fontSize })}>{goal}</span>
+    </div>
+  );
+}
