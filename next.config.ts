8b8f2aa3faff93b7517276d3ec8bf45b6d83c5e5
dy <gkekdus97@naver.com>
2025-02-25 09:40:10 +0900

    chore: 외부도메인 설정(Image/next) domains -> remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
