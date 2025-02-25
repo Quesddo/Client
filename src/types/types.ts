40adef6756b118266d4519d5f53dcaf0cb303c62
thgee <thgeee@naver.com>
2025-02-25 11:35:00 +0900

    chore: note 조회 api에서 goalId 필수로 넣어주도록 타입 수정

--- src/types/types.ts
+++ src/types/types.ts
@@ -868,7 +868,7 @@ export interface teamIdGoalsPostParams {
   createGoalBodyDto: CreateGoalBodyDto;
 }
 export interface teamIdNotesGetParams {
-  goalId?: number;
+  goalId: number;
   cursor?: number;
   size?: number;
 }
