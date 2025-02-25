a290731e294515109f1b06502ac702d037fd7bca
eunung <eunung24@gmail.com>
2025-02-25 14:46:51 +0900

    feat: react-quill 라이브러리 적용

--- src/views/note-creation-form/Editor.tsx
+++ src/views/note-creation-form/Editor.tsx
@@ -1,21 +1,49 @@
-import { useFormContext } from "react-hook-form";
+import "react-quill-new/dist/quill.snow.css";
+
+import dynamic from "next/dynamic";
+import { Controller, useFormContext } from "react-hook-form";
 
 import Button from "@/components/atoms/button/Button";
 import Input from "@/components/atoms/input/Input";
 
 import Toast from "./Toast";
 
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+
+const toolbarOptions = [
+  ["bold", "italic", "underline"],
+  [{ align: "" }, { align: "center" }, { align: "right" }],
+  [{ list: "bullet" }, { list: "ordered" }],
+  [{ color: [] }],
+  ["link"],
+];
+
+const modules = {
+  toolbar: toolbarOptions,
+};
+
 export default function Editor() {
-  const { register, watch } = useFormContext();
+  const { register, watch, control } = useFormContext();
   const content = watch("content");
 
   return (
     <div>
       <p className="pt-[12px] text-xs font-medium text-slate-800">
         공백포함 : 총 {content?.length || 0}자 | 공백제외 : 총{" "}
-        {content?.replace(/\s/gm, "").length}자
+        {content?.replace(/\s/gm, "").length || 0}자
       </p>
-      <textarea {...register("content", { required: true })}></textarea>
+      <Controller
+        control={control}
+        name="content"
+        render={({ field: { onChange, value } }) => (
+          <ReactQuill
+            theme="snow"
+            modules={modules}
+            onChange={onChange}
+            value={value}
+          ></ReactQuill>
+        )}
+      />
       <Toast />
       <div>
         <Button>link추가</Button>
