787f9bfb47d715a3f52e00e2ee6cfaf5ad28c3a9
thgee <thgeee@naver.com>
2025-02-27 03:38:41 +0900

    chore: [노트 모아보기] 매직넘버 분리

--- src/views/notes/note-list/NoteList.tsx
+++ src/views/notes/note-list/NoteList.tsx
@@ -2,6 +2,8 @@ import { TeamIdNotesGet200Response } from "@/types/types";
 
 import Card from "../card/Card";
 
+const SCROLL_TRIGGER_OFFSET = 4;
+
 interface noteListProps {
   notes: TeamIdNotesGet200Response["notes"] | undefined;
   inViewRef: (node?: Element | null) => void;
@@ -11,6 +13,10 @@ export default function NoteList({ notes, inViewRef }: noteListProps) {
   return (
     <div className="flex flex-col gap-4">
       {notes?.map(({ id, title, todo }, noteIdx, notes) => {
+        /* 미리 스크롤을 감지하기 위해 마지막 노트보다 SCROLL_TRIGGER_OFFSET 만큼 위에있는 노트에 스크롤 감지 블록을 위치시킴 */
+        const isShowScrollTrigger =
+          notes.length - (1 + SCROLL_TRIGGER_OFFSET) === noteIdx;
+
         return (
           <>
             <Card key={id}>
@@ -25,8 +31,7 @@ export default function NoteList({ notes, inViewRef }: noteListProps) {
               </Card.Body>
             </Card>
 
-            {/* 마지막 노트보다 4개 위에있는 노트에 스크롤 감지 블록을 위치시킴 */}
-            {notes.length - 5 === noteIdx && <div ref={inViewRef} />}
+            {isShowScrollTrigger && <div ref={inViewRef} />}
           </>
         );
       })}
