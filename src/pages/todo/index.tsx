f8d19609e3c5b02e72da39b130cd292f6d89876a
dy <gkekdus97@naver.com>
2025-02-26 17:31:25 +0900

    style: 할 일 페이지 마무리

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import PlusIcon from "@/components/atoms/plus-icon/PlusIcon";
-import TodoList from "@/components/organisms/todo-list/TodoList";
 import { useModalContext } from "@/contexts/InputModalContext";
 import { useDeleteTodo } from "@/hooks/todo/useDeleteTodo";
 import { useTodos } from "@/hooks/todo/useTodos";
@@ -10,27 +9,37 @@ import { cn } from "@/utils/cn";
 import DeletePopup from "@/views/todo/popup/DeletePopup";
 import TodoCreateForm from "@/views/todo/todo-create-form/TodoCreateForm";
 import TodoUpdateForm from "@/views/todo/todo-update-form/TodoUpdateForm";
+import Todos from "@/views/todo/todoPage/Todos";
+
+export const FILTER_TYPES = ["All", "Done", "To do"] as const;
 
 export default function TodoPage() {
   const { data } = useTodos();
-  const { isOpen, openModal } = useModalContext();
   const toggleTodoMutation = useUpdateTodo();
   const deleteTodoMutation = useDeleteTodo();
+  const { isOpen, openModal } = useModalContext();
 
   const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
-
-  const filterType = ["All", "Done", "To do"] as const;
-  const [filter, setFilter] = useState<(typeof filterType)[number]>("All");
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const filteredTodos = data?.todos.filter((todo) => {
-    if (filter === "Done") return todo.done;
-    if (filter === "To do") return !todo.done;
-    return true;
-  });
-  const handleToggleTodo = (todoId: number, isDone: boolean) => {
-    toggleTodoMutation.mutate({ todoId, data: { done: !isDone } });
-  };
+  const [filter, setFilter] = useState<(typeof FILTER_TYPES)[number]>("All");
+
+  const filteredTodos = useMemo(() => {
+    return (
+      data?.todos.filter((todo) => {
+        if (filter === "Done") return todo.done;
+        if (filter === "To do") return !todo.done;
+        return true;
+      }) || []
+    );
+  }, [data?.todos, filter]);
+
+  const handleToggleTodo = useCallback(
+    (todoId: number, isDone: boolean) => {
+      toggleTodoMutation.mutate({ todoId, data: { done: !isDone } });
+    },
+    [toggleTodoMutation],
+  );
 
   const handleOpenCreateModal = () => {
     setSelectedTodoId(null);
@@ -41,8 +50,8 @@ export default function TodoPage() {
     <div
       className={cn(
         "flex flex-col bg-slate-100 px-4 text-slate-800",
-        "h-[calc(100vh-64px)] pb-4",
-        "sm:h-[calc(100vh-16px)] sm:px-6 md:px-20",
+        "h-[calc(100vh-48px)]",
+        "sm:h-screen sm:px-6 md:px-20",
       )}
     >
       <div className="flex items-center justify-between sm:max-w-[636px] md:max-w-[792px]">
@@ -59,26 +68,14 @@ export default function TodoPage() {
         </button>
       </div>
 
-      <div className="h-full rounded-xl bg-white p-4 sm:max-w-[604px] md:max-w-[760px]">
-        {filterType.map((type) => (
-          <button
-            key={type}
-            onClick={() => setFilter(type)}
-            className={cn(
-              "mr-2 mb-4 rounded-full border border-slate-200 px-3 py-1 text-sm font-medium hover:shadow-sm",
-              filter === type ? "border-blue-500 bg-blue-500 text-white" : "",
-            )}
-          >
-            {type}
-          </button>
-        ))}
-        <TodoList
-          data={filteredTodos || []}
-          handleToggleTodo={handleToggleTodo}
-          setSelectedTodoId={setSelectedTodoId}
-          onOpenDeletePopup={() => setIsPopupOpen(true)}
-        />
-      </div>
+      <Todos
+        todos={filteredTodos}
+        filter={filter}
+        setFilter={setFilter}
+        handleToggleTodo={handleToggleTodo}
+        setSelectedTodoId={setSelectedTodoId}
+        setIsPopupOpen={() => setIsPopupOpen(true)}
+      />
 
       {isOpen && !selectedTodoId && <TodoCreateForm />}
       {isOpen && selectedTodoId && <TodoUpdateForm todoId={selectedTodoId} />}
