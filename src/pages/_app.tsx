5e1bac18e06787af0012d3ee1d0a7c3643dbb5b2
dy <gkekdus97@naver.com>
2025-02-26 13:09:09 +0900

    fix: 누락 부분 재수정 (CloseIcon삭제, InputModalProvider위치)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { InputModalProvider } from "@/contexts/InputModalContext";
+
 import Sidebar from "../views/layouts/template/Sidebar";
 import type { AppProps } from "next/app";
 
@@ -10,13 +12,14 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex h-screen flex-col overflow-y-hidden sm:flex-row">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto">
-          <Component {...pageProps} />
-        </main>
-      </div>
-      <div id="global-modal"></div>
+      <InputModalProvider>
+        <div className="flex h-screen flex-col overflow-y-hidden sm:flex-row">
+          <Sidebar />
+          <main className="flex-1 overflow-y-auto">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </InputModalProvider>
     </QueryClientProvider>
   );
 }
