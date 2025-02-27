2566f80b2abc8d344305e3f864d639bc82da0541
dy <gkekdus97@naver.com>
2025-02-27 17:05:41 +0900

    style: 고정너비를 비율로 수정

--- src/views/dashboard/my-progress/MyProgress.tsx
+++ src/views/dashboard/my-progress/MyProgress.tsx
@@ -1,6 +1,6 @@
 export default function MyProgress() {
   return (
-    <div className="h-[250px] rounded-xl bg-blue-500 text-white transition-shadow duration-300 hover:shadow-2xl sm:w-[588px]">
+    <div className="h-[250px] flex-1 rounded-xl bg-blue-500 text-white transition-shadow duration-300 hover:shadow-2xl">
       <h2 className="text-lg font-semibold">내 진행 상황</h2>
     </div>
   );
