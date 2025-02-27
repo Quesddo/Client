f985917b7ba352b01d28de7e33a2caabef9fb5a9
dy <gkekdus97@naver.com>
2025-02-27 13:46:30 +0900

    style: sm도 컨텐츠 꽉차게 (smd -> sm)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,8 +8,8 @@ export default function Dashboard() {
     <div className="flex h-full flex-col bg-slate-100 px-4 text-slate-800 sm:px-6 md:px-20">
       <PageTitle title="대시보드" className="sm:pt-6" isMobileFixed={true} />
 
-      <div className="smd:max-w-[1200px] smd:gap-5 flex h-full flex-grow flex-col gap-4 py-4 sm:max-w-[628px] sm:pt-0">
-        <div className="smd:gap-5 sm:flex sm:h-[250px] sm:justify-between">
+      <div className="smd:max-w-[1200px] smd:gap-5 flex h-full flex-grow flex-col gap-4 py-4 sm:pt-0">
+        <div className="sm:flex sm:h-[250px] sm:justify-between sm:gap-5">
           <RecentTodo />
           <MyProgress />
         </div>
--- src/views/dashboard/my-progress/MyProgress.tsx
+++ src/views/dashboard/my-progress/MyProgress.tsx
@@ -1,6 +1,6 @@
 export default function MyProgress() {
   return (
-    <div className="smd:w-[588px] h-[250px] rounded-xl bg-blue-500 text-white transition-shadow duration-300 hover:shadow-2xl sm:w-[306px]">
+    <div className="h-[250px] rounded-xl bg-blue-500 text-white transition-shadow duration-300 hover:shadow-2xl sm:w-[588px]">
       <h2 className="text-lg font-semibold">내 진행 상황</h2>
     </div>
   );
