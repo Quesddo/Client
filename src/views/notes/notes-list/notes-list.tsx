e7dbb37394b4e3208c8359d981351cb270cfe083
thgee <thgeee@naver.com>
2025-02-28 04:25:46 +0900

    refactor: noteList 파일 분리

---
+++ src/views/notes/notes-list/notes-list.tsx
@@ -0,0 +1,42 @@
+import { InfiniteData } from "@tanstack/react-query";
+
+import { TeamIdNotesGet200Response } from "@/types/types";
+
+import Card from "../card/card";
+
+interface noteListProps {
+  data: InfiniteData<TeamIdNotesGet200Response, unknown> | undefined;
+  inViewRef: (node?: Element | null) => void;
+}
+
+export default function NoteList({ data, inViewRef }: noteListProps) {
+  return (
+    <div className="flex flex-col gap-4">
+      {data?.pages.map((page, pageIdx) =>
+        page.notes.map(({ id, title, todo }, noteIdx, notes) => {
+          return (
+            <>
+              <Card key={id}>
+                <Card.Header />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Divider />
+                  <Card.Content>
+                    <Card.TodoStatus>
+                      {todo.done ? "Done" : "To do"}
+                    </Card.TodoStatus>
+                    <Card.TodoTitle>{todo.title}</Card.TodoTitle>
+                  </Card.Content>
+                </Card.Body>
+              </Card>
+
+              {/* 마지막 페이지의 마지막 노트보다 4개 위에있는 노트에 스크롤 감지 블록을 위치시킴 */}
+              {pageIdx === data.pages.length - 1 &&
+                notes.length - 5 === noteIdx && <div ref={inViewRef} />}
+            </>
+          );
+        }),
+      )}
+    </div>
+  );
+}
