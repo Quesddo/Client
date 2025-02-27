3db047325eda7b5b6f72bee7e27c78072bc5ba55
thgee <thgeee@naver.com>
2025-02-28 03:32:14 +0900

    feat: [노트 상세보기] 서스펜스 적용

--- src/apis/note/fetchNoteDetail.ts
+++ src/apis/note/fetchNoteDetail.ts
@@ -4,5 +4,8 @@ import instance from "../apiClient";
 
 export const fetchNoteDetail = async (
   noteId: number,
-): Promise<TeamIdNotesGet200ResponseNotesInner> =>
-  (await instance.get(`/notes/${noteId}`)).data;
+): Promise<TeamIdNotesGet200ResponseNotesInner | null> => {
+  if (noteId == null) return Promise.resolve(null);
+
+  return (await instance.get(`/notes/${noteId}`)).data;
+};
