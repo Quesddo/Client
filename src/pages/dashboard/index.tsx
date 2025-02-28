faa1ab4c5119f9cf1307e68cfdc8fc7ce70bd8bb
다염 <147478174+hdayeon@users.noreply.github.com>
2025-02-28 10:44:37 +0900

    style(TODO-165): 대시보드 기본 레이아웃 (#109)
    * feat: 대시보드 기본 레이아웃
    
    * style: 대시보드 기본 레이아웃
    
    * style: smd 고려 (md -> smd)
    
    * style: sm도 컨텐츠 꽉차게 (smd -> sm)
    
    * style: 좌측 사이드바 fixed 수정될 것 고려해 px 추가
    
    * style: 고정너비를 비율로 수정
    
    * style: smd에서 좌측 펼친 사이드바 간격 추가
    
    * feat: 타이틀 아이콘 공통 컴포넌트
    
    * style: 페이지 section 구분
    
    * style: main 태그로 변경

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,20 @@
+import PageTitle from "@/components/atoms/page-title/PageTitle";
+import GoalBasedTodo from "@/views/dashboard/goal-based-todo/GoalBasedTodo";
+import MyProgress from "@/views/dashboard/my-progress/MyProgress";
+import RecentTodo from "@/views/dashboard/recent-todo/RecentTodo";
+
+export default function Dashboard() {
+  return (
+    <main className="smd:pl-[357px] flex h-full flex-col bg-slate-100 px-4 text-slate-800 sm:pl-21">
+      <PageTitle title="대시보드" className="sm:pt-6" isMobileFixed={true} />
+
+      <div className="smd:max-w-[1200px] smd:gap-5 flex h-full flex-grow flex-col gap-4 py-4 sm:pt-0">
+        <div className="sm:flex sm:h-[250px] sm:justify-between sm:gap-5">
+          <RecentTodo />
+          <MyProgress />
+        </div>
+        <GoalBasedTodo />
+      </div>
+    </main>
+  );
+}
