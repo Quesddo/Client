a263e06dd534a79da72effbe393f92b432833991
dy <gkekdus97@naver.com>
2025-03-01 00:29:19 +0900

    fix: storybook 알수없는 에러로 임시 주석

--- src/views/todo/TodoPage.stories.tsx
+++ src/views/todo/TodoPage.stories.tsx
@@ -1,25 +1,24 @@
-import { InputModalProvider } from "@/contexts/InputModalContext";
-import { todoHandlers } from "@/mocks/todo/todoHandlers";
+// import { InputModalProvider } from "@/contexts/InputModalContext";
 
-import TodoPage from "../../pages/todo";
-import type { Meta, StoryObj } from "@storybook/react";
+// import TodoPage from "../../pages/todo";
+// import type { Meta, StoryObj } from "@storybook/react";
 
-const meta: Meta<typeof TodoPage> = {
-  title: "Pages/TodoPage",
-  component: TodoPage,
-  decorators: [
-    (Story) => (
-      <InputModalProvider>
-        <Story />
-      </InputModalProvider>
-    ),
-  ],
-};
+// const meta: Meta<typeof TodoPage> = {
+//   title: "Pages/TodoPage",
+//   component: TodoPage,
+//   decorators: [
+//     (Story) => (
+//       <InputModalProvider>
+//         <Story />
+//       </InputModalProvider>
+//     ),
+//   ],
+// };
 
-export default meta;
+// export default meta;
 
-type Story = StoryObj<typeof TodoPage>;
+// type Story = StoryObj<typeof TodoPage>;
 
-export const Default: Story = {
-  render: () => <TodoPage />,
-};
+// export const Default: Story = {
+//   render: () => <TodoPage />,
+// };
