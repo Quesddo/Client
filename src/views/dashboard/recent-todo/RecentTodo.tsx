c081c1095b9d302574f6923ff87c4218fb18271f
다염 <147478174+hdayeon@users.noreply.github.com>
2025-02-28 10:44:37 +0900

    style(TODO-165): 대시보드 기본 레이아웃 (#109)
    * feat: 대시보드 기본 레이아웃
    
    * style: 대시보드 기본 레이아웃
    
    * style: smd 고려 (md -> smd)
    
    * style: sm도 컨텐츠 꽉차게 (smd -> sm)
    
    * style: 좌측 사이드바 fixed 수정될 것 고려해 px 추가
    
    * style: 고정너비를 비율로 수정
    
    * style: smd에서 좌측 펼친 사이드바 간격 추가
    
    * feat: 타이틀 아이콘 공통 컴포넌트
    
    * style: 페이지 section 구분
    
    * style: main 태그로 변경

---
+++ src/views/dashboard/recent-todo/RecentTodo.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+
+export default function RecentTodo() {
+  const router = useRouter();
+  const handleShowAll = () => {
+    router.push("/todo");
+  };
+  return (
+    <section className="mb-4 h-[202px] flex-1 rounded-xl bg-white p-6 transition-shadow duration-300 hover:shadow-2xl">
+      <div className="flex h-10 justify-between">
+        <h2>최근 등록한 할 일</h2>
+        <button
+          onClick={handleShowAll}
+          className="text-sm font-medium text-slate-600"
+        >
+          모두보기
+        </button>
+      </div>
+
+      <div className="h-full max-h-[154px] overflow-y-hidden pt-[6px]">
+        <div className="flex h-full items-center justify-center text-sm font-normal text-slate-500">
+          최근에 등록한 할 일이 없어요
+        </div>
+      </div>
+    </section>
+  );
+}
