f5f6a6d40e59f43972e0f9c3475d6dc5d1ed2e63
eunung <eunung24@gmail.com>
2025-03-05 16:10:29 +0900

    fix: 내용 text 개수 오류 처리

--- src/views/note/note-form/Editor.tsx
+++ src/views/note/note-form/Editor.tsx
@@ -1,13 +1,31 @@
 import "react-quill-new/dist/quill.snow.css";
 
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { ForwardedRef, useEffect, useMemo, useRef } from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import ReactQuill, { Quill } from "react-quill-new";
 
 import { useModalContext } from "@/contexts/InputModalContext";
 import { cn } from "@/utils/cn";
 
-const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+interface ReactQuillNewProps extends ReactQuill.ReactQuillProps {
+  forwardedRef: ForwardedRef<ReactQuill> | undefined;
+}
+
+const ReactQuillEditor = dynamic(
+  async () => {
+    const { default: RQ } = await import("react-quill-new");
+
+    const ReactQuillNew = ({ forwardedRef, ...props }: ReactQuillNewProps) => (
+      <RQ ref={forwardedRef} {...props} formats={props.formats ?? undefined} />
+    );
+
+    return ReactQuillNew;
+  },
+  {
+    ssr: false,
+  },
+);
 
 const toolbarOptions = [
   ["bold", "italic", "underline"],
@@ -19,7 +37,8 @@ const toolbarOptions = [
 
 export default function Editor() {
   const { openModal } = useModalContext();
-  const { control } = useFormContext();
+  const methods = useFormContext();
+  const editorRef = useRef<ReactQuill>(null);
 
   const modules = useMemo(
     () => ({
@@ -33,27 +52,43 @@ export default function Editor() {
     [],
   );
 
+  const handleChangePlainText = () => {
+    methods.setValue(
+      "plainText",
+      editorRef.current?.getEditor().getText().replace(/\n+$/, ""),
+    );
+  };
+
   return (
     <Controller
-      control={control}
+      control={methods.control}
       name="content"
       rules={{ required: true }}
       render={({ field: { onChange, value } }) => (
-        <ReactQuill
-          theme="snow"
-          modules={modules}
-          onChange={onChange}
-          value={value}
-          placeholder="내용을 입력하세요"
-          className={cn(
-            "relative flex min-h-0 flex-1 flex-col-reverse gap-4",
-            "[&_.ql-toolbar]:rounded-[22px]",
-            "[&>.ql-container.ql-snow]:!border-none",
-            "[&>.ql-container]:min-h-0 [&>.ql-container]:!font-normal",
-            "[&_.ql-editor]:!p-0",
-            "[&_.ql-editor.ql-blank::before]:!left-0",
-          )}
-        ></ReactQuill>
+        <>
+          <ReactQuillEditor
+            forwardedRef={(el: ReactQuill | null) => {
+              editorRef.current = el;
+            }}
+            theme="snow"
+            modules={modules}
+            onChange={(value) => {
+              onChange(value);
+              handleChangePlainText();
+            }}
+            value={value}
+            placeholder="내용을 입력하세요"
+            className={cn(
+              "relative flex min-h-0 flex-1 flex-col-reverse gap-4",
+              "[&_.ql-toolbar]:rounded-[22px]",
+              "[&>.ql-container.ql-snow]:!border-none",
+              "[&>.ql-container]:min-h-0 [&>.ql-container]:!font-normal",
+              "[&_.ql-editor]:!p-0",
+              "[&_.ql-editor.ql-blank::before]:!left-0",
+            )}
+          ></ReactQuillEditor>
+          <input type="hidden" {...methods.register("plainText")} />
+        </>
       )}
     />
   );
