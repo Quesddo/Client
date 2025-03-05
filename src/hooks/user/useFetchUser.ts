840af9da0679e70c6c04d80af901cbf19f1e209f
Ralto13 <doorumi13@gmail.com>
2025-03-05 13:25:47 +0900

    refactor: fetch-user에 error 옵션 추가

--- src/hooks/user/useFetchUser.ts
+++ src/hooks/user/useFetchUser.ts
@@ -3,16 +3,16 @@ import { useQuery } from "@tanstack/react-query";
 import instance from "@/apis/apiClient";
 import { UserServiceResponseDto } from "@/types/types";
 
-export const useFetchUser = () => {
+export const useFetchUser = (throwOnError = false) => {
   const { data, isLoading } = useQuery<UserServiceResponseDto>({
     queryKey: ["user"],
     queryFn: async () => {
       const { data } = await instance.get("/user");
-
       return data;
     },
     retry: false,
     staleTime: 1000 * 60 * 60,
+    throwOnError: throwOnError,
   });
 
   return {
