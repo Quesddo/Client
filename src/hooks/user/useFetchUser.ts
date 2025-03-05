d51180e4331f4f4a6748dd29a1a04a9b2d6f0d86
Ralto13 <doorumi13@gmail.com>
2025-03-05 10:01:12 +0900

    refactor: 인증레이어 패치함수 변경

--- src/hooks/user/useFetchUser.ts
+++ src/hooks/user/useFetchUser.ts
@@ -4,12 +4,20 @@ import instance from "@/apis/apiClient";
 import { UserServiceResponseDto } from "@/types/types";
 
 export const useFetchUser = () => {
-  return useQuery<UserServiceResponseDto>({
+  const { data, isLoading } = useQuery<UserServiceResponseDto>({
     queryKey: ["user"],
     queryFn: async () => {
       const { data } = await instance.get("/user");
 
       return data;
     },
+    retry: false,
+    staleTime: 1000 * 60 * 60,
   });
+
+  return {
+    data,
+    isLoading,
+    isAuthenticated: !!data?.id,
+  };
 };
