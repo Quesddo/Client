3de751446e0684b68780c39f45c64646c223a076
thgee <thgeee@naver.com>
2025-03-10 09:49:01 +0900

    feat: [대시보드/목표별 할 일] 목표 API 데이터 바인딩

--- src/views/dashboard/goal-based-todo/GoalBasedTodo.tsx
+++ src/views/dashboard/goal-based-todo/GoalBasedTodo.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import { useRouter } from "next/router";
+import { useInView } from "react-intersection-observer";
 
-import PlusIcon from "@/components/atoms/plus-icon/PlusIcon";
 import TitleWithIcon from "@/components/atoms/title-with-icon/TitleWithIcon.tsx";
-import { useModalContext } from "@/contexts/InputModalContext";
-import { FilterType } from "@/types/todo";
+import { useInfiniteGoals } from "@/hooks/goal/useInfiniteGoals";
 
-import ProgressWrapper from "./components/ProgressBar";
-import TodoWrapper from "./components/TodoWrapper";
+import EmptyData from "./components/EmptyData";
+import GoalItem from "./components/GoalItem";
 
 interface GoalBasedTodoProps {
   handleToggleTodo: (todoId: number, isDone: boolean) => void;
@@ -20,20 +17,17 @@ export default function GoalBasedTodo({
   setSelectedTodoId,
   onOpenDeletePopup,
 }: GoalBasedTodoProps) {
-  const { openModal } = useModalContext();
-  const router = useRouter();
+  const { data, hasNextPage } = useInfiniteGoals();
+  const { inView, ref: inViewRef } = useInView();
 
-  const handleClickAddTodo = () => {
-    setSelectedTodoId(null);
-    openModal("createTodo");
-  };
-
-  const handleClickMore = () => {
-    router.push(`/goal/${1828}`);
-  };
+  // 목표가 하나도 없는 경우 예외처리
+  const hasGoals = data && data.goals.length > 0;
+  // 목표 데이터 파싱
+  const goals = data ? data.goals : [];
 
   return (
     <section className="rounded-xl bg-white p-6 transition-shadow duration-300 hover:shadow-2xl">
+      {/* 목표별 할 일 타이틀 */}
       <TitleWithIcon
         title="목표 별 할 일"
         imgUrl="/icons/flag-box.png"
@@ -41,56 +35,23 @@ export default function GoalBasedTodo({
         className="text-lg font-semibold"
       />
 
-      {/* 목표 리스트 */}
-      <ul className="mt-4 flex flex-col gap-4">
-        <li className="flex flex-col rounded-4xl bg-blue-50 p-6">
-          {/* 목표제목, 할 일 추가 */}
-          <div className="mb-2 flex justify-between">
-            <h3 className="text-lg font-bold text-slate-800">목표 제목</h3>
-            <button
-              className="flex items-center justify-center gap-1 text-blue-500 hover:brightness-90"
-              onClick={handleClickAddTodo}
-            >
-              <PlusIcon width={16} height={16} />
-              <span className="text-sm font-semibold">할 일 추가</span>
-            </button>
-          </div>
-
-          {/* 프로그래스바 */}
-          <ProgressWrapper goalId={1828} />
-
-          {/*  할 일 리스트 */}
-          <div className="my-4 flex flex-col gap-6 sm:flex-row">
-            {["todo", "done"].map((doneStatus, idx) => (
-              <TodoWrapper
-                handleToggleTodo={handleToggleTodo}
-                setSelectedTodoId={setSelectedTodoId}
-                onOpenDeletePopup={onOpenDeletePopup}
-                key={idx}
-                goalId={1828} // 임시로 넣어둠
-                doneStatus={doneStatus as FilterType}
-              />
-            ))}
-          </div>
-
-          {/* 더보기 */}
-          <button
-            onClick={handleClickMore}
-            className="flex h-[32px] w-[120px] items-center justify-center self-center rounded-2xl bg-white transition hover:scale-105"
-          >
-            <span className="ml-3 text-sm font-semibold text-slate-700">
-              자세히 보기
-            </span>
-            <Image
-              src="/icons/ic_arrow_right.svg"
-              alt={"arrow_right"}
-              width={20}
-              height={20}
-              layout="fixed"
+      {hasGoals ? (
+        <ul className="mt-4 flex flex-col gap-4">
+          {goals.map((goal) => (
+            <GoalItem
+              goal={goal}
+              key={goal.id}
+              handleToggleTodo={handleToggleTodo}
+              setSelectedTodoId={setSelectedTodoId}
+              onOpenDeletePopup={onOpenDeletePopup}
             />
-          </button>
-        </li>
-      </ul>
+          ))}
+        </ul>
+      ) : (
+        <EmptyData type="goal" />
+      )}
+
+      {/* 목표 리스트 */}
     </section>
   );
 }
