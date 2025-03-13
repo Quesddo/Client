b825ccc1fbf14b10b250a243a0b9d7ca289c2857
eunung <eunung24@gmail.com>
2025-03-13 15:01:50 +0900

    chore: useFetchTodo 구조화된 쿼리키 적용

--- src/hooks/todo/useFetchTodo.ts
+++ src/hooks/todo/useFetchTodo.ts
@@ -1,10 +1,11 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import todoApi from "@/apis/todoApi";
+import { queryKeys } from "@/query-keys";
 
 export const useFetchTodo = (todoId: number) => {
   return useSuspenseQuery({
-    queryKey: ["todo", todoId],
+    queryKey: queryKeys.todo.editNote(todoId).queryKey,
     queryFn: () => todoApi.fetchTodo(todoId),
   });
 };
