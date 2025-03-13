bd9142fec23ce8b879da5833af1a7412dfddf4d1
eunung <eunung24@gmail.com>
2025-03-13 11:52:17 +0900

    feat: useFetchTodo suspense 제거

--- src/hooks/todo/useFetchTodo.ts
+++ src/hooks/todo/useFetchTodo.ts
@@ -1,9 +1,9 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import todoApi from "@/apis/todoApi";
 
 export const useFetchTodo = (todoId: number) => {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: ["todo", todoId],
     queryFn: () => todoApi.fetchTodo(todoId),
   });
