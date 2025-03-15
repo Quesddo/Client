392730ebd5e1f73c4059a786caddbdc3102b023f
thgee <thgeee@naver.com>
2025-03-15 11:53:44 +0900

    refactor: [노트 상세] 임베드 API 호출 로직useQuery로 변경

--- src/hooks/note/useCanEmbed.ts
+++ src/hooks/note/useCanEmbed.ts
@@ -1,25 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchCanEmbed } from "@/apis/canEmbedApi";
+import { queryKeys } from "@/query-keys";
 
 /**
  * 임베드 가능 여부 체크하는 훅
  */
 export const useCanEmbed = (linkUrl?: string | null) => {
-  const [canEmbed, setCanEmbed] = useState<boolean | null>(null);
-
-  const getCanEmbed = async (link: string) => {
-    const { data } = await axios.get(`/api/check-iframe?url=${link}`);
-    return data.canEmbed;
-  };
-
-  // 임베드 가능하면 canEmbed = true
-  useEffect(() => {
-    if (!linkUrl) {
-      return;
-    }
-
-    getCanEmbed(linkUrl).then((res) => setCanEmbed(res));
-  }, [linkUrl]);
+  const { data: canEmbed } = useQuery({
+    queryKey: queryKeys.embed.canEmbed(linkUrl).queryKey,
+    queryFn: () => fetchCanEmbed(linkUrl),
+    enabled: !!linkUrl,
+    staleTime: 1000 * 60 * 60, // 자주 바뀌지 않는 데이터라 1시간 staleTime 설정함
+  });
 
   return { canEmbed };
 };
